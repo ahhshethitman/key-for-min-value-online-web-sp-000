f2460ef6ab667383244b3a65bf1dd70d430b690f
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-12 21:11:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,22 +4,24 @@
 
 
 
+
+
 def key_for_min_value(name_hash)
-  if name_hash.length == 0 
+  if name_hash.length == 0
     return nil
-  else 
-    new_array = [] 
-    name_hash.each do |key, value| 
-    new_array << value #stores each value into a new array 
-  end 
-  
-  lowest_num = new_array[0] #creates new var that will later be used as the lowest number
-  new_array.each do |value|
-    lowest_num = value if value < lowest_num 
+  else
+    array = []
+    name_hash.each do |key, value|
+      array << value
+    end
+    min_num = array[0]
+    array.each do |value|
+      min_num = value if value < min_num
+    end
+    name_hash.each do |key, value|
+      if min_num == value
+        return key
+      end
+    end
   end
-  name_hash.each do |key, value 
-  if lowest_num == value 
-    return key
-  end
-end
 end
